1b65b40dd5d87880b35936b656a84768e291af9d
artworkhex <artworkhex@gmail.com>
2023-09-10 23:29:42 +0700

    update 10

--- src/script.js
+++ src/script.js
@@ -31,3 +31,4 @@ searchInput.addEventListener("keyup", (event) => {
         }
     }
 });
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./*.html",
     './**/*.html',
     './KOSmecone - about/index.html',
-    './KOSmecone - detail/index.html',
+    './KOSmecone - detail/detail.html',
     './KOSmecone - login/index.html',
     './KOSmecone - login/KOSmecone - loginn/index.html',
     './KOSmecone - login/KOSmecone - regist/index.html',
